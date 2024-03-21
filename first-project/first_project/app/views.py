788d7ab9fc5e96a0cd95011a53a9dc4f6203a21d
Roman Karpov <rvk.go.1010@gmail.com>
2024-03-21 13:56:55 +0700

    first commit

---
+++ first-project/first_project/app/views.py
@@ -0,0 +1,31 @@
+import os
+import datetime
+
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+
+
+def home_view(request):
+    template_name = 'app/home.html'
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
+    }
+
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    current_time = datetime.datetime.now()
+    msg = f'Текущее время: {current_time}'
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    list_of_files = os.listdir()
+    msg = f'Список файлов рабочей директории: {list_of_files}'
+    return HttpResponse(msg)
